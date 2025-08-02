1c68dd66064016eb43dded44e57e727cc8b6e5ab
Ahmad Mirza Rafiq Azmi <139027185+Amierza@users.noreply.github.com>
2025-08-03 01:25:25 +0700

    debug-3

--- service/user_service.go
+++ service/user_service.go
@@ -592,7 +592,7 @@ func (us *UserService) UpdateTransactionTicket(ctx context.Context, req dto.Upda
 		transaction.TransactionStatus = "settlement"
 		loc, err := time.LoadLocation("Asia/Jakarta")
 		if err != nil {
-			return dto.ErrParseTime
+			loc = time.FixedZone("UTC+7", 7*60*60)
 		}
 		settlementTime, err := time.ParseInLocation("2006-01-02 15:04:05", req.SettlementTime, loc)
 		if err != nil {
